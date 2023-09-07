25f06e803569132122dc670807bdc3436b89a694
Erin Guenther <eringuenther@econ1-129-72-edu.int.colorado.edu>
2023-09-07 13:53:09 -0600

    added comments

--- APPM 4600/Labs/Lab1/Lab1Exercises1.py
+++ APPM 4600/Labs/Lab1/Lab1Exercises1.py
@@ -4,17 +4,22 @@ import math
 import matplotlib.pyplot as plt
 
 # (1)
+# initialize 50 equally spaced values between 0 and pi using two different built in functions
 x = np.linspace(0, np.pi, 50)
 y = np.arange(0,np.pi, 50)
 
 #(2)/(3)
+# list the first 3 entries of the x array using splicing
 print("The first 3 entires of x are", x[0:3])
 
 # (4)
+# create an array of 10 to the power of -1 to -10
 w = 10**(-np.linspace(1,10,10))
 
+# create a new array of integers from 1 to the length of the w array
 x = np.arange(1,len(w)+1)
 
+#plot x on the x-axis and w on the y-axis and label the axes
 plt.plot(x,w)
 plt.ylabel('w')
 plt.xlabel('x')
