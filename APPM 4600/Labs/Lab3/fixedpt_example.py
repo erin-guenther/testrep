00cbe017d0e7dcf973e2d55f867c3da1c3e31e88
Erin Guenther <eringuenther@econ1-131-3-edu.int.colorado.edu>
2023-09-14 14:10:32 -0600

    Question 3 on the lab

--- APPM 4600/Labs/Lab3/fixedpt_example.py
+++ APPM 4600/Labs/Lab3/fixedpt_example.py
@@ -4,27 +4,43 @@ import numpy as np
 def driver():
 
 # test functions 
-     f1 = lambda x: 1+0.5*np.sin(x)
-# fixed point is alpha1 = 1.4987....
+     #f1 = lambda x: x*(1+(7-x**5)/x**2)**3
 
-     f2 = lambda x: 3+2*np.sin(x)
-#fixed point is alpha2 = 3.09... 
+     #f2 = lambda x: x - (x**5-7)/x**2
+
+     f3 = lambda x: x - (x**5-7)/5*x**4
+
+     f4 = lambda x: x- (x**5-7)/12
 
      Nmax = 100
-     tol = 1e-6
+     tol = 1e-10
 
 # test f1 '''
-     x0 = 0.0
-     [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
-     print('the approximate fixed point is:',xstar)
-     print('f1(xstar):',f1(xstar))
-     print('Error message reads:',ier)
+     x0 = 1
+     #[xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+     #print('the approximate fixed point is:',xstar)
+     #print('f1(xstar):',f1(xstar))
+     #print('Error message reads:',ier)
     
 #test f2 '''
-     x0 = 0.0
-     [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+     x0 = 1
+     #[xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+     #print('the approximate fixed point is:',xstar)
+     #print('f2(xstar):',f2(xstar))
+     #print('Error message reads:',ier)
+
+#test f3 '''
+     x0 = 1
+     #[xstar,ier] = fixedpt(f3,x0,tol,Nmax)
+     #print('the approximate fixed point is:',xstar)
+     #print('f3(xstar):',f2(xstar))
+     #print('Error message reads:',ier)
+
+#test f4 '''
+     x0 = 1
+     [xstar,ier] = fixedpt(f4,x0,tol,Nmax)
      print('the approximate fixed point is:',xstar)
-     print('f2(xstar):',f2(xstar))
+     print('f4(xstar):',f4(xstar))
      print('Error message reads:',ier)
 
 
@@ -52,3 +68,9 @@ def fixedpt(f,x0,tol,Nmax):
     
 
 driver()
+
+
+#Discussion:
+# Fixed point converges for f4, but diverges for f1,f2,f3
+# This could be due to the fact that f1-f3 are divided by exponents and if we start with a small x0 the function value
+# could quickly increase
